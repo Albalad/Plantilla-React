0f2cfa4e556d42ae2d020f273fa1d3fa5a2ccb10
Albalad <alvaro.albalad@gmail.com>
2024-11-15 20:13:23 +0100

    plantilla react completa

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../styles/index.css'
-import Home from './components/Home.jsx'
+
+import Layout from './Layout.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <Layout />
   </StrictMode>,
 )
